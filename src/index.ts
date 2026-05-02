f191a67d2224151aaf7a294b211f013d4980eed6
Xuby <diazxavier27@gmail.com>
2026-05-02 18:39:32 -0400

    Add backend scanner functionality and update exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { extractVariables, analyzeNestedStructures } from './variableAnalyzer';
 export { extractVariableName } from './diagnosticMessage';
+export {
+  scanBackendOccurrences,
+  normalizeTemplateKey,
+  identifierAtOffset,
+} from './backendScanner';
+export type { BackendLang, BackendVarOccurrence } from './backendScanner';
